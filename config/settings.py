5ed75f1956d65a0a43f665950422cbdc7e557574
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2026-04-04 17:06:51 +0200

    fix: Asignación de clases para generar y controlar la autenticación en la configuración del proyecto

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Creación de métodos de autenticación
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
